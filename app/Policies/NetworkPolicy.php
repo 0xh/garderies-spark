898da1eac64993b3db4db77d2f822cfe69c01de6
Simon <simon.rapin@gmail.com>
2018-09-15 23:51:49 +0200

    -added: netowork policy

---
+++ app/Policies/NetworkPolicy.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Network;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class NetworkPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view the network.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Network  $network
+     * @return mixed
+     */
+    public function view(User $user, Network $network)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create networks.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the network.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Network  $network
+     * @return mixed
+     */
+    public function update(User $user, Network $network)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the network.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Network  $network
+     * @return mixed
+     */
+    public function delete(User $user, Network $network)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the network.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Network  $network
+     * @return mixed
+     */
+    public function restore(User $user, Network $network)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the network.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Network  $network
+     * @return mixed
+     */
+    public function forceDelete(User $user, Network $network)
+    {
+        //
+    }
+}
