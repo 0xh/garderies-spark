79d1bf335ac30076f648cf8515f17f7ba304cdce
Simon <simon.rapin@gmail.com>
2018-09-25 15:33:48 +0200

    -updated: display the user's own bookings

--- app/User.php
+++ app/User.php
@@ -72,6 +72,11 @@ class User extends SparkUser
         return $this->hasMany('App\Booking', 'substitute_id', 'id');
     }
 
+    public function ownBookings()
+    {
+        return $this->hasMany('App\Booking', 'user_id', 'id');
+    }
+
     public function bookingRequests()
     {
         return $this->hasMany('App\BookingRequest', 'substitute_id', 'id');
