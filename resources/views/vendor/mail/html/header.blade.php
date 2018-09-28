55ddfa5d4d3ecd6c6faae597f59db1b37c2df7e0
Simon <simon.rapin@gmail.com>
2018-09-28 16:47:31 +0200

    -updated: logo url

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}">
-            <img src="https://garderies.app/img/logo_garderies.png" alt="{{ $slot }}" width="160" height="40">
+            <img src="{{asset('/img/logo_garderies.png')}}" alt="{{ $slot }}" width="160" height="40">
         </a>
     </td>
 </tr>
