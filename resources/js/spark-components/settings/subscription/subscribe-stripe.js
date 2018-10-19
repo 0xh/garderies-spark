c3c160eb36b2a3006307599dc882850bb38ac750
Simon <simon.rapin@gmail.com>
2018-10-19 16:32:26 +0200

    -added: success alert when subscribed

--- resources/js/spark-components/settings/subscription/subscribe-stripe.js
+++ resources/js/spark-components/settings/subscription/subscribe-stripe.js
@@ -1,12 +1,26 @@
 var base = require('settings/subscription/subscribe-stripe');
 
+import swal from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
 Vue.component('spark-subscribe-stripe', {
     mixins: [base, require('./../mixins/vat')],
     mounted() {
         this.form.country = 'CH';
 
         Bus.$on('updateUser', function () {
-           window.location.reload();
+
+            swal({
+                type: "success",
+                text: "Nous vous remercions pour votre souscription, veuillez patienter quelques secondes le temps que nous préparions votre compte.",
+                timer: 5000,
+                onOpen: () => {
+                    swal.showLoading();
+                },
+                onAfterClose: () => {
+                    window.location.reload();
+                }
+            });
         });
     }
 });
